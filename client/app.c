7fea3639315dfc03630dea2b52c4df2a98e98fe4
NicolasCendron <nicolas_cendron@hotmail.com>
2020-09-07 23:12:39 -0300

    first

---
+++ client/app.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char **argv){
+
+    if(argc < 5){
+            fprintf(stderr,"Please follow this template.\n ./app <username> <groupname> <server_ip_address> <port>\n");
+            exit(1); //No file name
+        }
+
+    char *username = argv[1];
+    char *groupname = argv[2];
+    char *ip = argv[3];
+    char *port = argv[4];
+
+    printf("\nuser %s",username);
+    printf("\ngroup %s",groupname);
+    printf("\nip %s",ip);
+    printf("\nport %s",port);
+
+
+}
---
+++ client/runClient.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+make clean
+make
+./app user group 192.168.0.1 5000
