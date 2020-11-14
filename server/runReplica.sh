1f332cbf25c72fb7617b7fae6fdbf63ea397061d
Nicolas Cendron <nicolas_cendron@hotmail.com>
2020-11-14 10:30:23 -0200

    Arrumado para o debugger rodar automaticamente

--- server/runReplica.sh
+++ server/runReplica.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 make -f MakeReplica clean
 make -f MakeReplica
-gdb ./database/replica
+#./database/replica       # Rodar Normalmente a Replica
+gdb -ex run ./database/replica  # Rodar MODO DEBUGGER a Replica. (CTRL-Z para sari no terminal)
